eb7621781242d641fdec2af0f204c2c1caa097b0
Amanda Phillips <amanda@phillipsdev.org>
2018-08-28 18:14:37 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Amanda Phillips.</p>
+    <p>This is the first time I've done something with Gatsby!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
